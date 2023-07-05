fc43adac099130fc06f83a9fef5eaf6b04f13844
utkirbek <utkirbek_qutfiddinov@mail.ru>
2023-07-05 22:23:34 +0500

    User CRUD has been ended

--- src/main/java/com/example/jpaproject/controller/UserController.java
+++ src/main/java/com/example/jpaproject/controller/UserController.java
@@ -3,11 +3,10 @@ package com.example.jpaproject.controller;
 import com.example.jpaproject.model.User;
 import com.example.jpaproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Author: utkirbek.
@@ -21,7 +20,37 @@ public class UserController {
     UserRepository userRepository;
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public List<User> getUsers(){
+    public List<User> findAll(){
         return userRepository.findAll();
     }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public User getOne(@PathVariable Integer id){
+        Optional<User> optionalUser = userRepository.findById(id);
+        return optionalUser.orElseGet(User::new);
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public String save(@RequestBody User user){
+        userRepository.save(user);
+        return "Successfully added!";
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+    public String delete(@PathVariable Integer id){
+        userRepository.deleteById(id);
+        return "Succesfully deleted!";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.PUT)
+    public String update(@RequestBody User user){
+        Optional<User> optionalUser=userRepository.findById(user.getId());
+        if (optionalUser.isPresent()){
+            userRepository.save(user);
+            return "Updated succesfully!";
+        }else {
+            return "User not found!";
+        }
+    }
+
 }
